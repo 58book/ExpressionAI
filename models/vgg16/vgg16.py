003d7cd3ce4dce8ef8ef3868ad47ce6f8077aca5
Rounak Chawla <rxc542@case.edu>
2023-03-10 18:45:06 -0500

    Retrained model with different structure, computing confusion matrix and test metrics

--- models/vgg16/vgg16.py
+++ models/vgg16/vgg16.py
@@ -14,8 +14,8 @@ df = df.apply(lambda row:
 train_df = df[1:27001]
 val_df = df[27001:]
 
-train_ds = tf.data.Dataset.from_tensor_slices((tf.convert_to_tensor(train_df['pixels'].tolist(), dtype=tf.float32), tf.keras.utils.to_categorical(tf.convert_to_tensor(train_df['emotion'].tolist(), dtype=tf.int32), num_classes=7))).shuffle(27000).batch(BATCH_SIZE)
-val_ds = tf.data.Dataset.from_tensor_slices((tf.convert_to_tensor(val_df['pixels'].tolist(), dtype=tf.float32), tf.keras.utils.to_categorical(tf.convert_to_tensor(val_df['emotion'].tolist(), dtype=tf.int32), num_classes=7))).shuffle(1000).batch(BATCH_SIZE)
+train_ds = tf.data.Dataset.from_tensor_slices((tf.image.grayscale_to_rgb(tf.convert_to_tensor(train_df['pixels'].tolist(), dtype=tf.float32)), tf.keras.utils.to_categorical(tf.convert_to_tensor(train_df['emotion'].tolist(), dtype=tf.int32), num_classes=7))).shuffle(27000).batch(BATCH_SIZE)
+val_ds = tf.data.Dataset.from_tensor_slices((tf.image.grayscale_to_rgb(tf.convert_to_tensor(val_df['pixels'].tolist(), dtype=tf.float32)), tf.keras.utils.to_categorical(tf.convert_to_tensor(val_df['emotion'].tolist(), dtype=tf.int32), num_classes=7))).shuffle(1000).batch(BATCH_SIZE)
 
 vgg_conv_base = tf.keras.applications.vgg16.VGG16(
     weights='imagenet',
@@ -25,9 +25,8 @@ vgg_conv_base.trainable = True
 for layer in vgg_conv_base.layers[:-4]:
     layer.trainable = False
 
-inputs = tf.keras.Input(shape=(48,48,1))
-x = tf.keras.layers.Lambda(tf.image.grayscale_to_rgb)(inputs)
-x = tf.keras.applications.vgg16.preprocess_input(x)
+inputs = tf.keras.Input(shape=(48,48,3))
+x = tf.keras.applications.vgg16.preprocess_input(inputs)
 x = vgg_conv_base(x)
 x = tf.keras.layers.Flatten()(x)
 x = tf.keras.layers.Dense(512, activation='relu')(x)
